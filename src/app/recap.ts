eae7c07fea6c8337b9e031e928faad74f3ffd3db
Adhemar <adhemarduran@gmail.com>
2022-08-17 23:36:17 -0400

    create node version file and an introduction to TS

---
+++ src/app/recap.ts
@@ -0,0 +1,7 @@
+const username: string = 'adhemar';
+const sum = (a: number, b: number): number => {
+  return a + b;
+}
+class person {
+  constructor(public age: number, public lastName: string) {}
+}
